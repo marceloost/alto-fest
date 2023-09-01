99f085cba1ed07119d39a5cf9fd8c3e8ba9f8eb5
marceloost <104876213+marceloost@users.noreply.github.com>
2023-09-01 15:32:58 -0300

    up

---
+++ alto-fest/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Alto Fest</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>9° Alto Fest</h1>
+        <h2>Venha para a 9° Alto Fest!</h2>
+        <p>Muitas atrações esperam por você.</p>
+        <p>Dias 12, 13, 14 e 15 de outubro de 2023.</p>
+        <figure>
+            <img src="imagens/imagem1.jpeg" alt="Descrição da imagem 1">
+            <figcaption>Informações sobre a imagem 1</figcaption>
+        </figure>
+        <figure>
+            <img src="imagens/imagem2.jpeg" alt="Descrição da imagem 1">
+            <figcaption>Informações sobre a imagem 2</figcaption>
+        </figure>
+        <figure>
+            <img src="imagens/imagem3.jpeg" alt="Descrição da imagem 1">
+            <figcaption>Informações sobre a imagem 3</figcaption>
+        </figure>
+    </div>
+</body>
+</html>
