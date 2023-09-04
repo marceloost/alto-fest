231e1dca0f7c4dbe8cacc5d0ffec691b85ddb026
marceloost <104876213+marceloost@users.noreply.github.com>
2023-09-04 14:29:43 -0300

    up

--- alto-fest/index.php
+++ alto-fest/index.php
@@ -20,18 +20,30 @@
         <div class="">
             <p>Dias 12, 13, 14 e 15 de outubro de 2023.</p>
         </div>
-        <figure>
-            <img src="imagens/imagem1.jpeg" alt="Descrição da imagem 1">
-            <figcaption>Informações sobre a imagem 1</figcaption>
-        </figure>
-        <figure>
-            <img src="imagens/imagem2.jpeg" alt="Descrição da imagem 2">
-            <figcaption>Informações sobre a imagem 2</figcaption>
-        </figure>
-        <figure>
-            <img src="imagens/imagem3.jpeg" alt="Descrição da imagem 3">
-            <figcaption>Informações sobre a imagem 3</figcaption>
-        </figure>
+    
+        <div class="sub-container">
+            <div class="image-card">
+                <img src="imagens/imagem1.jpeg" alt="Descrição da imagem 1">
+                <div class="info">
+                    <h3>Informações sobre a imagem 1</h3>
+                    <p>Adicionar aqui uma descrição detalhada da imagem 1.</p>
+                </div>
+            </div>
+            <div class="image-card">
+                <img src="imagens/imagem2.jpeg" alt="Descrição da imagem 2">
+                <div class="info">
+                    <h3>Informações sobre a imagem 2</h3>
+                    <p>Adicionar aqui uma descrição detalhada da imagem 2.</p>
+                </div>
+            </div>
+            <div class="image-card">
+                <img src="imagens/imagem3.jpeg" alt="Descrição da imagem 3">
+                <div class="info">
+                    <h3>Informações sobre a imagem 3</h3>
+                    <p>Adicionar aqui uma descrição detalhada da imagem 3.</p>
+                </div>
+            </div>
+        </div>
     </div>
 </body>
 </html>
