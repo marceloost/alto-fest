4ba66c91c040c1a751eb8e232eb4bbc4d8ddbdea
marceloost <104876213+marceloost@users.noreply.github.com>
2023-09-04 17:02:32 -0300

    up

--- alto-fest/index.php
+++ alto-fest/index.php
@@ -25,24 +25,52 @@
             <div class="image-card">
                 <img src="imagens/imagem1.jpeg" alt="Descrição da imagem 1">
                 <div class="info">
-                    <h3>Informações sobre a imagem 1</h3>
-                    <p>Adicionar aqui uma descrição detalhada da imagem 1.</p>
+                    <h3>9° Alto Fest</h3>
+                    <p>A Alto Fest é a festa maior do Município de Alto Feliz e festeja a integração das etnias (alemã e italiana) que contribuíram para a formação de nosso povo, valorizando os potenciais culturais, sociais e econômicos do Município.
+</p>
                 </div>
             </div>
             <div class="image-card">
                 <img src="imagens/imagem2.jpeg" alt="Descrição da imagem 2">
                 <div class="info">
-                    <h3>Informações sobre a imagem 2</h3>
-                    <p>Adicionar aqui uma descrição detalhada da imagem 2.</p>
+                    <h3>9° Alto Fest</h3>
+                    <p>Em 2023, a 9ª Alto Fest contará com diversas atrações culturais, como artistas nacionais, bandas regionais e locais, bandinhas típicas e apresentações culturais de grupos de dança, de orquestras, corais etc.
+A festa também contará com espaços para alimentação, com comida típica alemã e italiana, e para exposição da indústria, do comércio e da agricultura, além de cerveja artesanal e tradicional, produzida em Alto Feliz, visando valorizar, em especial, o que é produzido em nosso Município.
+</p>
                 </div>
             </div>
             <div class="image-card">
                 <img src="imagens/imagem3.jpeg" alt="Descrição da imagem 3">
                 <div class="info">
-                    <h3>Informações sobre a imagem 3</h3>
-                    <p>Adicionar aqui uma descrição detalhada da imagem 3.</p>
+                    <h3>9° Alto Fest</h3>
+                    <p>A edição de 2023 acontece na Praça Municipal Professor Ervino Aloysio Flach, localizada nos arredores da Prefeitura Municipal de Alto Feliz.
+A festa está sendo preparada de modo a oferecer aos nossos munícipes e ao público visitante ampla infraestrutura, atrações diversificadas e o reconhecimento dos potenciais da nossa terra.
+</p>
                 </div>
             </div>
+        </div>
+        <div class="programacao">
+            <p class="titulo-prog">Confira a programação aqui:</p>
+
+            <div class="image-folder">
+                <img src="imagens/folder01.png" alt="Folder">
+                <div class="info">
+                    <h3></h3>
+                    <p></p>
+                </div>
+            </div>
+
+            <div class="image-folder">
+                <img src="imagens/folder02.png" alt="Folder">
+                <div class="info">
+                    <h3></h3>
+                    <p></p>
+                </div>
+            </div>
+        </div>
+
+        <div class="links">
+            <p>Confira todos os detalhes da 9° Alto Fest em nossas páginas do <a href="https://www.facebook.com/profile.php?id=100095396420664" class="link">Facebook</a> e <a href="https://www.instagram.com/altofest_altofeliz/" class="link">Instagram</a>.</p>
         </div>
     </div>
 </body>
