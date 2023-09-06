60e5917583d968ff75c15e49c4cad2343d406ce2
marceloost <104876213+marceloost@users.noreply.github.com>
2023-09-06 16:31:43 -0300

    up

--- alto-fest/index.php
+++ alto-fest/index.php
@@ -14,10 +14,10 @@
         <div class="subtitulo">
             <h2>Venha para a 9° Alto Fest!</h2>
         </div>
-        <div class="">
+        <div class="enunciado">
             <p>Muitas atrações esperam por você.</p>
         </div>
-        <div class="">
+        <div class="enunciado">
             <p>Dias 12, 13, 14 e 15 de outubro de 2023.</p>
         </div>
     
@@ -26,26 +26,32 @@
                 <img src="imagens/imagem1.jpeg" alt="Descrição da imagem 1">
                 <div class="info">
                     <h3>9° Alto Fest</h3>
-                    <p>A Alto Fest é a festa maior do Município de Alto Feliz e festeja a integração das etnias (alemã e italiana) que contribuíram para a formação de nosso povo, valorizando os potenciais culturais, sociais e econômicos do Município.
-                    </p>
+                    <div class="descricao">
+                        <p>A Alto Fest é a festa maior do Município de Alto Feliz e festeja a integração das etnias (alemã e italiana) que contribuíram para a formação de nosso povo, valorizando os potenciais culturais, sociais e econômicos do Município.
+                        </p>
+                    </div>
                 </div>
             </div>
             <div class="image-card">
                 <img src="imagens/imagem2.jpeg" alt="Descrição da imagem 2">
                 <div class="info">
                     <h3>9° Alto Fest</h3>
-                    <p>Em 2023, a 9ª Alto Fest contará com diversas atrações culturais, como artistas nacionais, bandas regionais e locais, bandinhas típicas e apresentações culturais de grupos de dança, de orquestras, corais etc.
-                    A festa também contará com espaços para alimentação, com comida típica alemã e italiana, e para exposição da indústria, do comércio e da agricultura, além de cerveja artesanal e tradicional, produzida em Alto Feliz, visando valorizar, em especial, o que é produzido em nosso Município.
-                    </p>
+                    <div class="descricao">
+                        <p>Em 2023, a 9ª Alto Fest contará com diversas atrações culturais, como artistas nacionais, bandas regionais e locais, bandinhas típicas e apresentações culturais de grupos de dança, de orquestras, corais etc.
+                        A festa também contará com espaços para alimentação, com comida típica alemã e italiana, e para exposição da indústria, do comércio e da agricultura, além de cerveja artesanal e tradicional, produzida em Alto Feliz, visando valorizar, em especial, o que é produzido em nosso Município.
+                        </p>
+                    </div>
                 </div>
             </div>
             <div class="image-card">
                 <img src="imagens/imagem3.jpeg" alt="Descrição da imagem 3">
                 <div class="info">
                     <h3>9° Alto Fest</h3>
-                    <p>A edição de 2023 acontece na Praça Municipal Professor Ervino Aloysio Flach, localizada nos arredores da Prefeitura Municipal de Alto Feliz.
-                    A festa está sendo preparada de modo a oferecer aos nossos munícipes e ao público visitante ampla infraestrutura, atrações diversificadas e o reconhecimento dos potenciais da nossa terra.
-                    </p>
+                    <div class="descricao">
+                        <p>A edição de 2023 acontece na Praça Municipal Professor Ervino Aloysio Flach, localizada nos arredores da Prefeitura Municipal de Alto Feliz.
+                        A festa está sendo preparada de modo a oferecer aos nossos munícipes e ao público visitante ampla infraestrutura, atrações diversificadas e o reconhecimento dos potenciais da nossa terra.
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
